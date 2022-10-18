3211b5deb20b86b3ccd08b754f8facc832e43258
chrisdavidhutchinson <chris.d.hutchinson@live.co.uk>
2022-10-18 10:01:49 +0100

    Updated flow-of-control docs with correct indentations

--- flow-of-control/dice_game.rb
+++ flow-of-control/dice_game.rb
@@ -1,32 +1,34 @@
 # Dice Game
+# Both players have two 6 sided dice
+# First player to two win to games wins
 
 player_1_wins = 0
 player_2_wins = 0
 
 while true do 
 
-    player_1_dice = rand(13)
-    player_2_dice = rand(13)
-    puts player_1_dice
-    puts player_2_dice
+  player_1_dice = rand(13)
+  player_2_dice = rand(13)
+  puts player_1_dice
+  puts player_2_dice
 
-    if player_1_dice > player_2_dice
-        puts "Player 1 - game win"
-        player_1_wins = player_1_wins + 1
-    elsif player_1_dice < player_2_dice
-        puts "Player 2 - game win"
-        player_2_wins = player_2_wins + 1
-    else 
-        puts "Draw"
-    end
+  if player_1_dice > player_2_dice
+    puts "Player 1 - game win"
+    player_1_wins = player_1_wins + 1
+  elsif player_1_dice < player_2_dice
+    puts "Player 2 - game win"
+    player_2_wins = player_2_wins + 1
+  else 
+    puts "Draw"
+  end
 
-    if player_1_wins == 2
-        puts "Player 1 wins - first to 2"
-        break
-    elsif player_2_wins == 2
-        puts "Player 2 wins - first to 2"
-        break
-    end
+  if player_1_wins == 2
+    puts "Player 1 wins - first to 2"
+    break
+  elsif player_2_wins == 2
+    puts "Player 2 wins - first to 2"
+    break
+  end
 
 end
 
